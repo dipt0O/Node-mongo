21d14a94ee3242727590b77197f5f02e9cc85f08
dipt0o <6sabit@gmail.com>
2021-03-25 20:28:22 +0600

    dynamic-api-done

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 
-
+const users =["Asad","Based","Suhana",'Liza',"Dipto", 'shujon'];
 const rootCall = (req, res) =>{res.send('Thank you very much')}
 app.get('/',(req, res)=>{
-    res.send("thank you for calling me")
+    const fruit ={
+        product: 'ada',
+        price: 220
+    }
+    res.send(fruit);
+});
+app.get('/fruit/banana', (req, res) =>{
+    res.send({fruit:'banana', quantity:1000, price: 10000})
 })
+
+app.get('users/:id', (req, res) => {
+    const userId = req.params.id;
+    const name = users[usersId];
+    res.send(name);
+});
 
 app.listen(3000, ()=> console.log('listening port 3000'));
