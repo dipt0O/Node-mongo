6da383b913ee9cc43c73f4a8bddfc6236a0b7e02
dipt0o <6sabit@gmail.com>
2021-04-05 19:41:57 +0600

    done

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
-const app = express()
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+
+app.use(cors());
+app.use(bodyParser.json());
 
 const users =["Asad","Based","Suhana",'Liza',"Dipto", 'shujon'];
-const rootCall = (req, res) =>{res.send('Thank you very much')}
+
+// const rootCall = (req, res) =>{res.send('Thank you very much')}
 app.get('/',(req, res)=>{
     const fruit ={
         product: 'ada',
@@ -14,10 +22,18 @@ app.get('/fruit/banana', (req, res) =>{
     res.send({fruit:'banana', quantity:1000, price: 10000})
 })
 
-app.get('users/:id', (req, res) => {
-    const userId = req.params.id;
-    const name = users[usersId];
-    res.send(name);
+app.get('/users/:id', (req, res) => {
+//   console.log(users.params);
+    const id = req.params.id;
+    const name = users[id];
+    res.send(id, name);
 });
+
+//Post
+app.post('/addUser',(req, res)=>{
+ console.log('post send');
+});
+
+
 
 app.listen(3000, ()=> console.log('listening port 3000'));
